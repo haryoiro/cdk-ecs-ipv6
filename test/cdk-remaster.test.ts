f3fba9b3c552dc75c1b1b73212543c43e1d3453b
haryoiro <mizusecocolte@gmail.com>
2024-06-23 16:25:18 +0900

    Initial commit

---
+++ test/cdk-remaster.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkRemaster from '../lib/cdk-remaster-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-remaster-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkRemaster.CdkRemasterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
